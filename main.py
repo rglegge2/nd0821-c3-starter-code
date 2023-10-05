31c34bc4add86d1c3b03f31adfa4c59afde5f4eb
Legge II, Robert G <rglegge2@gmail.com>
2023-10-05 17:28:49 -0500

    try Field again

--- main.py
+++ main.py
@@ -1,39 +1,38 @@
 import logging
 import pickle
-from typing import Any
 
 import pandas as pd
-from fastapi import FastAPI, Body
+from fastapi import FastAPI
 from pydantic import BaseModel, Field
-from typing_extensions import Annotated
 
-from starter.ml.model import inference
 from starter.ml.data import process_data
+from starter.ml.model import inference
 from starter.train_model import cat_features
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 
+app = FastAPI()
+
 
 # Declare the data object with its components and their type.
 class InputData(BaseModel):
-    age: int = Field(examples=[39])
-    workclass: str
-    fnlgt: int
-    education: str
-    education_num: int
-    marital_status: str
-    occupation: str
-    relationship: str
-    race: str
-    sex: str
-    capital_gain: int
-    capital_loss: int
-    hours_per_week: int
-    native_country: str
+    age: int = Field(example=39)
+    workclass: str = Field(example='State-gov')
+    fnlgt: int = Field(example=77516)
+    education: str = Field(example='Bachelors')
+    education_num: int = Field(example=13)
+    marital_status: str = Field(example='Never-married')
+    occupation: str = Field(example='Adm-clerical')
+    relationship: str = Field(example='Not-in-family')
+    race: str = Field(example='White')
+    sex: str = Field(example='Male')
+    capital_gain: int = Field(example=2174)
+    capital_loss: int = Field(example=0)
+    hours_per_week: int = Field(example=40)
+    native_country: str = Field(example='United_States')
 
 
-app = FastAPI()
 model_path = "model/trained_model.pkl"
 encoder_path = "model/trained_encoder.pkl"
 lb_path = "model/trained_lb.pkl"
@@ -48,22 +47,7 @@ async def hello_world():
 
 
 @app.post("/inference")
-async def run_inference(input_data: Annotated[InputData, Body(default=None, examples=[{
-                    'age': 39,
-                    'workclass': 'State-gov',
-                    'fnlgt': 77516,
-                    'education': 'Bachelors',
-                    'education_num': 13,
-                    'marital_status': 'Never-married',
-                    'occupation': 'Adm-clerical',
-                    'relationship': 'Not-in-family',
-                    'race': 'White',
-                    'sex': 'Male',
-                    'capital_gain': 2174,
-                    'capital_loss': 0,
-                    'hours_per_week': 40,
-                    'native_country': 'United_States'
-                }])]) -> dict:
+async def run_inference(input_data: InputData) -> dict:
     input_dict = input_data.dict()
     logger.info(input_dict)
     input_df = pd.DataFrame.from_dict(input_dict, orient='index').T
